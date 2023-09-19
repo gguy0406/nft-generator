64190d0c210c97c6107ff8397986cc73f7ec6add
Trung D. Ngo <gguy0406@gmail.com>
2023-09-19 16:03:58 +0700

    chore: add c0 setting

--- src/constant.ts
+++ src/constant.ts
@@ -2,6 +2,7 @@ import {join as pathJoin} from 'path';
 
 export const collectionDir = pathJoin(__dirname, '..', 'collection');
 export const traitsDir = pathJoin(collectionDir, 'traits');
+export const fontsDir = pathJoin(collectionDir, 'fonts');
 export const outputDir = pathJoin(collectionDir, 'output');
 export const outputImageDir = pathJoin(outputDir, 'images');
 export const outputMetadataDir = pathJoin(outputDir, 'metadata');
--- src/setting.ts
+++ src/setting.ts
@@ -23,38 +23,74 @@ export interface ColorSetting {
   colorSets: Array<ColorSetting['defaultSet']>;
 }
 
+// ch0pch0p c0 setting
 export const setting: GeneratorSetting = {
   resetOutputs: true,
-  shuffling: true,
-  indexStep: 200,
-  imgSize: 2000,
+  // shuffling: true,
+  indexStep: 100,
+  imgSize: 595.28,
   resolution: 150,
-  randomTimes: 30,
-  setsGenerator: 'multiplication',
-  traits: ['Background', 'Body', 'Cloth', 'Hand', 'Self', 'Head', 'Hair', 'Face', 'Ear'],
-  randomTraits: ['Background', 'Ear'],
-  hiddenTraits: ['Background', 'Body', 'Ear'],
+  randomTimes: 10000,
+  setsGenerator: 'randomization',
+  traits: ['Background', 'Body', 'Self', 'Head', 'Face', 'Hair', 'Hands', 'Accessories'],
+  // randomTraits: ['Background', 'Body'],
+  // hiddenTraits: ['Background', 'Body', 'Ear'],
   syncColor: {
-    traits: ['Body', 'Hand', 'Head', 'Face', 'Ear'],
+    traits: ['Body', 'Head', 'Hands'],
     types: ['Main color', 'Shadow color'],
-    defaultSet: {'Main color': '#FFC68E', 'Shadow color': '#D18154'},
+    defaultSet: {'Main color': '#FEB8B0', 'Shadow color': '#BA828F'},
     colorSets: [
-      {'Main color': '#FFC68E', 'Shadow color': '#D18154'},
-      {'Main color': '#FFCAB5', 'Shadow color': '#FFA080'},
-      {'Main color': '#FFECF1', 'Shadow color': '#FFDAE4'},
-      {'Main color': '#9C7828', 'Shadow color': '#5F3806'},
-      {'Main color': '#AA4A37', 'Shadow color': '#71150C'},
-      {'Main color': '#7C00D9', 'Shadow color': '#3C00B9'},
+      {'Main color': '#FEB8B0', 'Shadow color': '#BA828F'},
+      {'Main color': '#F4CD98', 'Shadow color': '#DBA86B'},
+      {'Main color': '#EFAC5E', 'Shadow color': '#C17434'},
+      {'Main color': '#A56628', 'Shadow color': '#7F4B1C'},
     ],
   },
-  constraintSetting: {
-    Ear: {Earrings: {disjoin: {Hand: ['Spinning']}}},
-    Face: {
-      StarFace: {
-        join: {Self: ['Evil', 'Planet']},
-        disjoin: {Hair: ['TiffanyCowboy']},
-      },
-    },
-    Hair: {CyanFuzz: {join: {Hand: ['BluePot']}}},
-  },
+  // constraintSetting: {
+  //   Ear: {Earrings: {disjoin: {Hand: ['Spinning']}}},
+  //   Face: {
+  //     StarFace: {
+  //       join: {Self: ['Evil', 'Planet']},
+  //       disjoin: {Hair: ['TiffanyCowboy']},
+  //     },
+  //   },
+  //   Hair: {CyanFuzz: {join: {Hand: ['BluePot']}}},
+  // },
 };
+
+// ch0pch0p c1 setting
+// export const setting: GeneratorSetting = {
+//   resetOutputs: true,
+//   shuffling: true,
+//   indexStep: 200,
+//   imgSize: 2000,
+//   resolution: 150,
+//   randomTimes: 30,
+//   setsGenerator: 'multiplication',
+//   traits: ['Background', 'Body', 'Cloth', 'Hand', 'Self', 'Head', 'Hair', 'Face', 'Ear'],
+//   randomTraits: ['Background', 'Ear'],
+//   hiddenTraits: ['Background', 'Body', 'Ear'],
+//   syncColor: {
+//     traits: ['Body', 'Hand', 'Head', 'Face', 'Ear'],
+//     types: ['Main color', 'Shadow color'],
+//     defaultSet: {'Main color': '#FFC68E', 'Shadow color': '#D18154'},
+//     colorSets: [
+//       {'Main color': '#FFC68E', 'Shadow color': '#D18154'},
+//       {'Main color': '#FFCAB5', 'Shadow color': '#FFA080'},
+//       {'Main color': '#FFECF1', 'Shadow color': '#FFDAE4'},
+//       {'Main color': '#9C7828', 'Shadow color': '#5F3806'},
+//       {'Main color': '#AA4A37', 'Shadow color': '#71150C'},
+//       {'Main color': '#7C00D9', 'Shadow color': '#3C00B9'},
+//     ],
+//   },
+//   constraintSetting: {
+//     Ear: {Earrings: {disjoin: {Hand: ['Spinning']}}},
+//     Face: {
+//       StarFace: {
+//         join: {Self: ['Evil', 'Planet']},
+//         disjoin: {Hair: ['TiffanyCowboy']},
+//       },
+//     },
+//     Hair: {CyanFuzz: {join: {Hand: ['BluePot']}}},
+//   },
+// };
